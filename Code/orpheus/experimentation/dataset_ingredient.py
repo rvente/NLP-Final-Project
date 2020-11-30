21a7696d2eca74b144a9c033cacfac3a92968e29
Blake Vente <venteralph@gmail.com>
2020-11-29 19:04:59 -0500

    add new temporary dataset ingredient

--- Code/orpheus/experimentation/dataset_ingredient.py
+++ Code/orpheus/experimentation/dataset_ingredient.py
@@ -8,7 +8,7 @@ auth100docs50 = Ingredient('dataset')
 
 
 @auth100docs50.config
-def cfg():
+def a100_d50_cfg():
     filename = './data/100A50D.xlsx'
     col_labels = ["user_id", "review_contents"]
 
@@ -23,6 +23,26 @@ def load_data(filename, col_labels):
     #    cut off by feature selection (max_features)
     vectorizer = TfidfVectorizer()
     reviews = df['review_contents'].to_list()
+    authors = df['user_id'].to_list()
+    vectors = vectorizer.fit_transform(reviews)
+    print(df.head())
+    X_train, X_test, y_train, y_test = train_test_split(
+        vectors, authors, test_size=0.3, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+auth100docs50_L0_path = Ingredient('dataset')
+
+@auth100docs50_L0_path.config
+def a100_d50_l0_cfg():
+    filename = './data/100A50D_POS.pkl'
+    col_labels = ["user_id", "review_contents", "pos_tags"]
+    min_doc_frequency = .005
+
+@auth100docs50_L0_path.capture
+def a100_d50_l0_cfg_load_data(filename, col_labels, min_doc_frequency):
+    df = pd.read_pickle(filename)
+    vectorizer = TfidfVectorizer(min_df=min_doc_frequency)
+    reviews = df['pos_tags'].to_list()
     authors = df['user_id'].to_list()
     vectors = vectorizer.fit_transform(reviews)
     print(df.head())
