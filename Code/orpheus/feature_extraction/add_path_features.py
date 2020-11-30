34d99686f60db42c1b71bd49d829f8ce735efec1
Blake Vente <venteralph@gmail.com>
2020-11-30 18:18:09 -0500

    feature extractor runs with pos, bigrams, and l1 paths

--- Code/orpheus/feature_extraction/add_path_features.py
+++ Code/orpheus/feature_extraction/add_path_features.py
@@ -6,32 +6,43 @@
 # version.
 #
 # This program loads pickles and adds a 'pos_tags' column derived from the
-# 'Spacy.Docs' in the dataframe. It takes about 0.05 seconds per review on one
+# 'spacy.Docs' in the dataframe. It takes about 0.05 seconds per review on one
 # thread of a desktop Intel i5 4690K @ 3.5 GHz (a worst-case).
 
 
-from instance_parser import span_visitor, unigram_getter, doc_to_pos_string
+from instance_parser import (
+    doc_visitor, span_to_pos,
+    pos_nested_bigram_getter, l1_path_getter)
 import pandas as pd
 from tqdm import tqdm
 tqdm.pandas()
 
 
-DATASET_BASENAME = 'data/small'
+# DATASET_BASENAME = 'data/small'
+DATASET_BASENAME = 'data/100A50D'
+# DATASET = 'data/100A50D_with_doc.pkl'
 DATASET = f'{DATASET_BASENAME}_with_doc.pkl'
 DATASET_OUTNAME = f'{DATASET_BASENAME}__doc+pos.pkl'
-# DATASET = 'data/100A50D_with_doc.pkl'
-
-def pos_col_append(df: 'pd.DataFrame[documents]') -> 'MUTATES':
-    """MUTATE df with POS column; MUTATION for efficiency"""
-    df['pos_tags'] = df['documents'].progress_apply(doc_to_pos_string)
-
 
 if __name__ == "__main__":
     try:
         print("read start")
         df = pd.read_pickle(DATASET)
         print("read done")
-        pos_col_append(df)
+
+        def doc_to_pos(doc): return doc_visitor(doc, span_to_pos)
+
+        def doc_to_pos_bigram(doc):
+            return doc_visitor(doc, pos_nested_bigram_getter)
+
+        def doc_to_l1_paths(doc):
+            return doc_visitor(doc, l1_path_getter)
+
+        # mutate to add pos_tag unigrams
+        df['pos_tags'] = df['documents'].progress_apply(doc_to_pos)
+        # mutate to add pos_tag unigrams
+        df['nested_pos_bigrams'] = df['documents'].progress_apply(doc_to_pos_bigram)
+        df['path_pos_bigrams'] = df['documents'].progress_apply(doc_to_l1_paths)
         df.drop(columns=['documents'], inplace=True)
         print(df)
         df.to_pickle(DATASET_OUTNAME)
--- Code/orpheus/feature_extraction/instance_parser.py
+++ Code/orpheus/feature_extraction/instance_parser.py
@@ -5,25 +5,24 @@
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# This program will parse sentences and return trees.
+# This program stores utilities to convert tree representations to string
+# representations.
 
-
-import unittest
-
-import spacy
+import spacy 
 from benepar.spacy_plugin import BeneparComponent
 import pandas as pd
 from code import InteractiveConsole
 from pprint import pprint, pformat
 import warnings
-
+from typing import Callable, Tuple, Generator
 
 import tensorflow as tf
 tf.gfile = tf.io.gfile
 
+space_delimited_tokens = str
 
 # TODO: ensure there are no references and remove this function
-# def extract_unipath(doc: 'Spacy.Doc') -> 'List[List[Tuple[str]]]':
+# def extract_unipath(doc: 'spacy.Doc') -> 'List[List[Tuple[str]]]':
 
 #     # visit each node of the tree, yielding results of calling fn on the span
 #     def parse(span, fn) -> 'Tuple[str]':
@@ -39,19 +38,19 @@ tf.gfile = tf.io.gfile
 #     # reduce surrounding tuples so we have List[str]
 #     return by_sentence
 
-def doc_to_pos_string(doc: 'Spacy.Doc') -> 'str str ... str':
+def doc_to_pos_string(doc: 'spacy.Doc') -> 'space_delimited_tokens':
     """traverse, get unigrams, flatten into space separated pos_tags"""
 
     return " ".join(" ".join(span_visitor(sent, unigram_getter)) for sent in doc.sents)
 
 
-def doc_visitor(doc: 'Spacy.Doc', fn: 'Callable') -> 'str str ... str':
+def doc_visitor(doc: 'spacy.Doc', fn: 'Callable') -> 'space_delimited_tokens':
     """apply fn(Span) across document flatten into space separated tokens"""
 
     return " ".join(" ".join(span_visitor(sent, fn)) for sent in doc.sents)
 
 
-def span_visitor(span: 'Spacy.Span', fn: 'Callable') -> 'Tuple[str]':
+def span_visitor(span: 'spacy.Span', fn: 'Callable') -> 'Generator[spacy.Span, Callable, None]':
     """ visit all nodes in tree, applying fn on every node"""
     children = list(span._.children)
     yield fn(span)
@@ -59,19 +58,19 @@ def span_visitor(span: 'Spacy.Span', fn: 'Callable') -> 'Tuple[str]':
         yield from span_visitor(child, fn)
 
 
-def unigram_getter(x: 'Spacy.Span') -> 'str':
+def unigram_getter(x: 'spacy.Span') -> 'str':
     """get POS unigrams aka POS single pos label"""
     warnings.warn("please use span_to_pos instead")
     return span_to_pos(x)
 
 
-def span_to_pos(x: 'Spacy.Span') -> 'str':
+def span_to_pos(x: 'spacy.Span') -> 'str':
     """ ret POS from a span, if span leaf, take POS from base str"""
     # leaf nodes have no labels?? extract them from the string.
     return (x._.labels or (x._.parse_string.partition(" ")[0][1:], ))[0]
 
 
-def pos_nested_bigram_getter(x: 'Spacy.Span') -> 'str str ... str':
+def pos_nested_bigram_getter(x: 'spacy.Span') -> 'space_delimited_tokens':
     """returns all nested bigrams from x as space-separated tokens"""
     # leaf nodes have no labels?? extract them from the string.
     x_pos_tag = span_to_pos(x)
@@ -79,7 +78,7 @@ def pos_nested_bigram_getter(x: 'Spacy.Span') -> 'str str ... str':
     return " ".join(f"{x_pos_tag}↓{span_to_pos(child)}" for child in x_children)
 
 
-def l1_path_getter(x: 'Spacy.Span') -> 'str str ... str':
+def l1_path_getter(x: 'spacy.Span') -> 'space_delimited_tokens':
     """returns all POS paths of length 1 from node x as strings"""
     # leaf nodes have no labels?? extract them from the string.
     x_pos_tag = span_to_pos(x)
@@ -90,13 +89,14 @@ def l1_path_getter(x: 'Spacy.Span') -> 'str str ... str':
 
 
 if __name__ == '__main__':
-    # for unit tests, we import a small mock dataset
-    # pd.Dataframe['user_id','review_contents','documents']
+    """ for unit tests, we import a small mock dataset of the form
+    pd.Dataframe['user_id','review_contents','documents']
+    where 'documents' is a 'spacy.Doc """
 
     DATASET_BASENAME = 'data/small'
     DATASET = f'{DATASET_BASENAME}_with_doc.pkl'
     df = pd.read_pickle(DATASET)
-    doc: 'Spacy.Doc' = df['documents'][0]
+    doc: 'spacy.Doc' = df['documents'][0]
     for getter in [span_to_pos, l1_path_getter, pos_nested_bigram_getter]:
         print(getter)
         print(doc_visitor(doc, getter))
