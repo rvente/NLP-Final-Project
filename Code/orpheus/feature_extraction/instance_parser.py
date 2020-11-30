bc21df2fdcbfee63adf83f89d6685f428c496ea4
Blake Vente <venteralph@gmail.com>
2020-11-30 17:09:38 -0500

    wrote functions for nested pos_tag bigrams and pos paths of length 2

--- Code/orpheus/feature_extraction/instance_parser.py
+++ Code/orpheus/feature_extraction/instance_parser.py
@@ -15,6 +15,7 @@ from benepar.spacy_plugin import BeneparComponent
 import pandas as pd
 from code import InteractiveConsole
 from pprint import pprint, pformat
+import warnings
 
 
 import tensorflow as tf
@@ -38,10 +39,16 @@ tf.gfile = tf.io.gfile
 #     # reduce surrounding tuples so we have List[str]
 #     return by_sentence
 
-def doc_to_pos_string(doc: 'Spacy.Doc') -> 'str':
+def doc_to_pos_string(doc: 'Spacy.Doc') -> 'str str ... str':
     """traverse, get unigrams, flatten into space separated pos_tags"""
 
     return " ".join(" ".join(span_visitor(sent, unigram_getter)) for sent in doc.sents)
+
+
+def doc_visitor(doc: 'Spacy.Doc', fn: 'Callable') -> 'str str ... str':
+    """apply fn(Span) across document flatten into space separated tokens"""
+
+    return " ".join(" ".join(span_visitor(sent, fn)) for sent in doc.sents)
 
 
 def span_visitor(span: 'Spacy.Span', fn: 'Callable') -> 'Tuple[str]':
@@ -53,9 +60,9 @@ def span_visitor(span: 'Spacy.Span', fn: 'Callable') -> 'Tuple[str]':
 
 
 def unigram_getter(x: 'Spacy.Span') -> 'str':
-    """ ret POS from a span, if span leaf, take POS from base str"""
-    # leaf nodes have no labels?? extract them from the string.
-    return (x._.labels or (x._.parse_string.partition(" ")[0][1:], ))[0]
+    """get POS unigrams aka POS single pos label"""
+    warnings.warn("please use span_to_pos instead")
+    return span_to_pos(x)
 
 
 def span_to_pos(x: 'Spacy.Span') -> 'str':
@@ -64,12 +71,22 @@ def span_to_pos(x: 'Spacy.Span') -> 'str':
     return (x._.labels or (x._.parse_string.partition(" ")[0][1:], ))[0]
 
 
-def l1_path_getter(x: 'Spacy.Span') -> 'str str ... str':
-    """ unigram getter but returns paths of length 1 as strings"""
+def pos_nested_bigram_getter(x: 'Spacy.Span') -> 'str str ... str':
+    """returns all nested bigrams from x as space-separated tokens"""
     # leaf nodes have no labels?? extract them from the string.
     x_pos_tag = span_to_pos(x)
     x_children = x._.children
-    return " ".join(x_pos_tag+"↓"+span_to_pos(child) for child in x_children)
+    return " ".join(f"{x_pos_tag}↓{span_to_pos(child)}" for child in x_children)
+
+
+def l1_path_getter(x: 'Spacy.Span') -> 'str str ... str':
+    """returns all POS paths of length 1 from node x as strings"""
+    # leaf nodes have no labels?? extract them from the string.
+    x_pos_tag = span_to_pos(x)
+    x_par = x._.parent
+    par_pos_tag = span_to_pos(x_par) if x_par is not None else "ROOT"
+    upper_bigram = f"{x_pos_tag}↑{par_pos_tag}"
+    return (pos_nested_bigram_getter(x) + " " + upper_bigram).strip()
 
 
 if __name__ == '__main__':
@@ -80,7 +97,6 @@ if __name__ == '__main__':
     DATASET = f'{DATASET_BASENAME}_with_doc.pkl'
     df = pd.read_pickle(DATASET)
     doc: 'Spacy.Doc' = df['documents'][0]
-    s = list(span_visitor(list(doc.sents)[0], unigram_getter))
-    print(s)
-    t = list(span_visitor(list(doc.sents)[0], l1_path_getter))
-    print(t)
+    for getter in [span_to_pos, l1_path_getter, pos_nested_bigram_getter]:
+        print(getter)
+        print(doc_visitor(doc, getter))
