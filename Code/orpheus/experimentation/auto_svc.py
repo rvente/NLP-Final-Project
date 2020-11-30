e1354496c45f7c165f45b9c5ae40a39c10a3a31e
Blake Vente <venteralph@gmail.com>
2020-11-29 23:06:15 -0500

    running automation complete

--- Code/orpheus/experimentation/auto_svc.py
+++ Code/orpheus/experimentation/auto_svc.py
@@ -6,5 +6,11 @@ from l0_100a_50d import ex
 for m_df in [.001, .003, .005, .01, .1]:
     ex.run('svc', config_updates={
         'dataset.min_doc_freq': m_df,
-        'name': f'svc @min_df {m_df}'
+        'name': f'svc__min_df_{m_df}'
     })
+
+# for m_df in [.001, .003, .005, .01, .1]:
+#     ex.run('MultiNomNB', config_updates={
+#         'dataset.min_doc_freq': m_df,
+#         'name': f'nb__min_df_{m_df}'
+#     })
--- Code/orpheus/experimentation/l0_100a_50d.py
+++ Code/orpheus/experimentation/l0_100a_50d.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pandas as pd
-[]
 from sacred import Experiment, Ingredient
 from sacred.observers import FileStorageObserver
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 
 ex = Experiment('100a_50d_l0')
-# ex.observers.append(FileStorageObserver('logs'))
+ex.observers.append(FileStorageObserver('logs'))
 
 @ex.config
 def cfg():
@@ -27,7 +27,7 @@ def load_data(filename, min_doc_freq):
     df = pd.read_pickle(filename)
 
     vectorizer = TfidfVectorizer(min_df=min_doc_freq)
-    experimental_feature = df['pos_tags'].to_list()
+    experimental_feature = df['review_contents'].to_list()
     authors = df['user_id'].to_list()
     vectors = vectorizer.fit_transform(experimental_feature)
     print(df.head(), df.shape, sep="\n")
@@ -50,5 +50,20 @@ def svc(_log, name):
     ex.log_scalar("test.confusion", str(confusion))
     ex.log_scalar("test.f1_score", f1)
     _log.info(f"{name}")
+    _log.info(f"acc={accuracy}, f1={f1}")
+    return accuracy
+
+@ex.command
+def MultiNomNB(_log, name):
+    X_train, X_test, y_train, y_test = load_data()
+    clf = MultinomialNB(alpha=.01)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    confusion = confusion_matrix(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred, average="weighted")
+    ex.log_scalar("test.accuracy", accuracy)
+    ex.log_scalar("test.confusion", str(confusion))
+    ex.log_scalar("test.f1_score", f1)
     _log.info(f"acc={accuracy}, f1={f1}")
     return accuracy
