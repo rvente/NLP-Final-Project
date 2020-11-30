e94cbc8a90c6b5ad6f4567c3aa11cdeb6b7e5175
Blake Vente <venteralph@gmail.com>
2020-11-30 15:21:29 -0500

    add progress bars

--- Code/orpheus/feature_extraction/add_parse_tree.py
+++ Code/orpheus/feature_extraction/add_parse_tree.py
@@ -5,8 +5,10 @@
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# This program will take a df of user_id, review_contents and add another
-# columnd containing the
+# This program will take a df of user_id, review_contents and add another column
+# containing the parse tree features as a Spacy.Doc
+#
+# TODO: Future Work: this is 'trivially multi-threadable' using Python's Dask
 
 from collections import Counter
 
@@ -24,12 +26,14 @@ from sklearn.svm import LinearSVC
 from sklearn.utils import shuffle
 
 from instance_parser import PathExtractor
+from tqdm import tqdm
+tqdm.pandas()
 
 
 DATA_DIR_NAME = 'data/'
 # DATA_FILE_NAME = "50A30D.csv"
 # DATA_FILE_NAME = "100A50D.csv"
-DATA_FILE_NAME = "Sample.xlsx"
+DATA_FILE_NAME = "100A50D.xlsx"
 COL_LABELS = ["user_id", "review_contents"]
 
 
@@ -50,15 +54,18 @@ def experiment(N_TRIALS, N_SPLITS, classifier, X, y):
 
 if __name__ == "__main__":
     # read file with two cols, "user_id" and "review_contents"
-    df = pd.read_excel(DATA_DIR_NAME+DATA_FILE_NAME, names=COL_LABELS, header = None)
+    df = pd.read_excel(DATA_DIR_NAME+DATA_FILE_NAME, names=COL_LABELS)
 
     extractor = PathExtractor()
+
+    df = df.head(10)
     df = df.astype('string')
-    df['documents'] = df['review_contents'].map(extractor.extract_doc_tree)
+    df['documents'] = df['review_contents'].progress_map(extractor.extract_doc_tree)
 
     try:
         # df.to_hdf("100A50D_with_doc.h5", key='df')
-        df.to_pickle("Sample_POS.pkl")
+        # df.to_pickle("100A50D_with_doc.pkl")
+        df.to_pickle("Sample_with_doc.pkl")
     except Exception as e:
         print(e)
 
--- Code/orpheus/feature_extraction/add_path_features.py
+++ Code/orpheus/feature_extraction/add_path_features.py
@@ -5,28 +5,32 @@
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# This program loads pickles and adds a 'pos_tags' column.
+# This program loads pickles and adds a 'pos_tags' column derived from the
+# 'Spacy.Docs' in the dataframe. It takes about 0.05 seconds per review on one
+# thread of a desktop Intel i5 4690K @ 3.5 GHz (a worst-case).
 
-
-import pandas as pd
 
 from instance_parser import span_visitor, unigram_getter
+import pandas as pd
+from tqdm import tqdm
+tqdm.pandas()
 
-DATASET = 'Sample_POS.pkl'
-DATASET = 'data/Sample_POS.pkl'
-CTR = 1
+
+DATASET_BASENAME = 'data/small'
+DATASET = f'{DATASET_BASENAME}_with_doc.pkl'
+DATASET_OUTNAME = f'{DATASET_BASENAME}__doc+pos.pkl'
+# DATASET = 'data/100A50D_with_doc.pkl'
+
 
 def doc_to_pos_string(doc: 'Spacy.Doc') -> 'str':
     """traverse, get unigrams, flatten into space separated pos_tags"""
-    global CTR
-    CTR += 1
-    print(CTR/5000)
+
     return " ".join(" ".join(span_visitor(sent, unigram_getter)) for sent in doc.sents)
 
 
 def pos_col_append(df: 'pd.DataFrame[documents]') -> 'MUTATES':
     """MUTATE df with POS column; MUTATION for efficiency"""
-    df['pos_tags'] = df['documents'].apply(doc_to_pos_string)
+    df['pos_tags'] = df['documents'].progress_apply(doc_to_pos_string)
 
 
 if __name__ == "__main__":
@@ -37,7 +41,7 @@ if __name__ == "__main__":
         pos_col_append(df)
         df.drop(columns=['documents'], inplace=True)
         print(df)
-        df.to_pickle("data/Sample_POS.pkl")
+        df.to_pickle(DATASET_OUTNAME)
 
     except Exception as e:
         print(e)
